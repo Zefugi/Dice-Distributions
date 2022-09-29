2cba8a733ee7ffe760efb56ccf36fb4986b69826
Guanomancer <78472401+Guanomancer@users.noreply.github.com>
2022-09-29 12:19:31 +0200

    Changed graph formatting to include % chance

--- DiceDistributions/DiceDistributions/Program.cs
+++ DiceDistributions/DiceDistributions/Program.cs
@@ -1,7 +1,12 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.IO.IsolatedStorage;
+using System.Text;
 
-var distribution = SumChanceDistribution(new int[] { 4, 4 });
-OutputDistributionGraph(distribution);
+var dice = new int[] { 4, 6 };
+var distribution = SumChanceDistribution(dice);
+var totalCombinations = TotalCombinations(dice);
+Console.WriteLine($"Dice combinations: {totalCombinations}");
+OutputDistributionGraph(distribution, totalCombinations);
 
 void OutputDistribution(int[] distribution)
 {
@@ -9,20 +14,26 @@ void OutputDistribution(int[] distribution)
         Console.WriteLine($"{string.Format("{0:00}", i)}  {string.Format("{0:00}", distribution[i])}");
 }
 
-void OutputDistributionGraph(int[] distribution)
+const string SPACER = "    ";
+const string INT_FORMAT = " {0:00} ";
+const string PERCENT_FORMAT = "{0:00.0} %";
+const string COLUMN_SHAPE = @" /\ ";
+const string ROW_SHAPE = " -- ";
+void OutputDistributionGraph(int[] distribution, int totalCombinations)
 {
     var str = new StringBuilder();
     int maxValue = MaxValue(distribution);
-    for(int value = maxValue; value > 0; value--)
+    for (int value = maxValue; value > 0; value--)
     {
-        str.AppendFormat("{0:00}  ", value);
+        str.AppendFormat(INT_FORMAT, value);
         for (int i = 1; i < distribution.Length; i++)
-            str.Append((distribution[i] >= value) ? @"/\  " : "--  ");
+            str.Append((distribution[i] >= value) ? COLUMN_SHAPE : ROW_SHAPE);
+        str.AppendFormat(PERCENT_FORMAT, (float)value * 100f / totalCombinations);
         str.AppendLine();
     }
-    str.Append("    ");
+    str.Append(SPACER);
     for (int i = 1; i < distribution.Length; i++)
-        str.AppendFormat("{0:00}  ", i);
+        str.AppendFormat(INT_FORMAT, i);
     Console.WriteLine(str);
 }
 
@@ -70,4 +81,12 @@ int MaxValue(int[] values)
         if (values[i] > max)
             max = values[i];
     return max;
+}
+
+int TotalCombinations(int[] dice)
+{
+    int totalCombinations = dice[0];
+    for (int d = 1; d < dice.Length; d++)
+        totalCombinations *= dice[d];
+    return totalCombinations;
 }
